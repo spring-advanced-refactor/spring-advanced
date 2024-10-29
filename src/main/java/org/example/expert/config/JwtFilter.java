0e2c658a3eb18cebf061fc708cfb590143849bc1
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-29 11:53:25 +0900

    3-3: clean-up

--- src/main/java/org/example/expert/config/JwtFilter.java
+++ src/main/java/org/example/expert/config/JwtFilter.java
@@ -29,48 +29,41 @@ public class JwtFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-
         String url = httpRequest.getRequestURI();
-
         if (url.startsWith("/auth")) {
             chain.doFilter(request, response);
+
             return;
         }
-
         String bearerJwt = httpRequest.getHeader("Authorization");
-
         if (bearerJwt == null) {
-            // 토큰이 없는 경우 400을 반환합니다.
             httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "JWT 토큰이 필요합니다.");
+
             return;
         }
-
         String jwt = jwtUtil.substringToken(bearerJwt);
-
         try {
-            // JWT 유효성 검사와 claims 추출
             Claims claims = jwtUtil.extractClaims(jwt);
             if (claims == null) {
                 httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 JWT 토큰입니다.");
+
                 return;
             }
-
             UserRole userRole = UserRole.valueOf(claims.get("userRole", String.class));
-
             httpRequest.setAttribute("userId", Long.parseLong(claims.getSubject()));
             httpRequest.setAttribute("email", claims.get("email"));
             httpRequest.setAttribute("userRole", claims.get("userRole"));
-
             if (url.startsWith("/admin")) {
                 // 관리자 권한이 없는 경우 403을 반환합니다.
                 if (!UserRole.ADMIN.equals(userRole)) {
                     httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "관리자 권한이 없습니다.");
+
                     return;
                 }
                 chain.doFilter(request, response);
+
                 return;
             }
-
             chain.doFilter(request, response);
         } catch (SecurityException | MalformedJwtException e) {
             log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.", e);
